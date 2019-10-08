b05a92b48d7b24dce71a1ac2c78041f75da61c02
Tobias Schultze <webmaster@tubo-world.de>
2019-10-08 11:30:59 +0200

    update for jms serializer v2/v3

--- src/AdapterSerializationContextFactory.php
+++ src/AdapterSerializationContextFactory.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Liip\Serializer\Adapter\JMS;
 
 use JMS\Serializer\ContextFactory\SerializationContextFactoryInterface;
+use JMS\Serializer\SerializationContext;
 
 class AdapterSerializationContextFactory implements SerializationContextFactoryInterface
 {
-    public function createSerializationContext()
+    public function createSerializationContext(): SerializationContext
     {
         return new AdapterSerializationContext();
     }
